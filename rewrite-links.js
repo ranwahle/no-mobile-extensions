c2f97bfac1e60430d026597143b1bf95fdbe844c
Ran Wahle <ran@Pec-Ran-Wahle.local>
2021-07-16 14:18:49 +0300

    Initial commit

---
+++ rewrite-links.js
@@ -0,0 +1,16 @@
+function overrideLinks() {
+    const mobilePrefix = 'm.';
+    const anchors = document.querySelectorAll(`a[href*="${mobilePrefix}`)
+    anchors.forEach(anchor => anchor.href = anchor.href.replace(mobilePrefix, 'www.'));
+}
+
+    const interval = 2000;
+    const caller = () => {
+        setTimeout(() => {
+            caller();
+        }, interval);
+        overrideLinks();
+    };
+    caller();
+
+
